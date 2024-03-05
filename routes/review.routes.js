f4fab83c04ae52cf0888d930280d6a902f4e1384
arisar86 <ari.sariannidis@gmail.com>
2024-03-05 21:07:59 +0100

    review.routes added

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -1,12 +1,26 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
-const Cafe = require("../models/Review.model.js");
+const Review = require("../models/Review.model.js");
 
-const {isAuthenticated} = require ("./middleware/jwt.middleware.js");
+const { isAuthenticated } = require("./middleware/jwt.middleware.js");
 
 //POST
+router.post("/reviews", isAuthenticated, (req, res, next) => {
+  Review.create(req.body)
+    .then((newReview) => res.status(201).json(newReview))
+    .catch((err) => {
+      next(err);
+    });
+});
 
 //READ
-
+router.get("/reviews", (req, res, next) => {
+  Review.find()
+    .populate("reviews") // populate cafes instead?????
+    .then((allReviews) => res.status(200).json(allReviews))
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
