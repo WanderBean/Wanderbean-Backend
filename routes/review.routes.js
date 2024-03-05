29c8c16ad511a7902b8de24ab417096f0b5ca1f6
Simona Wurst <simona_wu@yahoo.de>
2024-03-05 18:56:56 +0100

    Add CRUD in cafe.routes.js

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
-const Cafe = require("./models/Review.model.js");
+const Cafe = require("../models/Review.model.js");
 
 const {isAuthenticated} = require ("./middleware/jwt.middleware.js");
 
