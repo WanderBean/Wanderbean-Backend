065f4697d5490a49e1939c926cb3daef66df6a2d
Simona Wurst <simona_wu@yahoo.de>
2024-03-05 18:28:42 +0100

    Initial setup cafe & review routes

---
+++ routes/cafe.routes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const Cafe = require("./models/Cafe.model.js");
+
+const {isAuthenticated} = require ("./middleware/jwt.middleware.js");
+
+//POST
+
+//READ
+
+//UPDATE
+
+//DELETE
+
+module.exports = router;
---
+++ routes/review.routes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const Cafe = require("./models/Review.model.js");
+
+const {isAuthenticated} = require ("./middleware/jwt.middleware.js");
+
+//POST
+
+//READ
+
+
+module.exports = router;
