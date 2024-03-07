f9f36ba7ca98b8d99b1cc2f5c110a6fa9d766f06
arisar86 <ari.sariannidis@gmail.com>
2024-03-07 14:40:14 +0100

    typos corrected

--- models/Cafe.model.js
+++ models/Cafe.model.js
@@ -1,32 +1,34 @@
-const { Schema, model } = require("mongoose");   //const Schema = mongoose.Schema;
+const { Schema, model } = require("mongoose"); //const Schema = mongoose.Schema;
 
-const cafeSchema = new Schema(
+const cafeSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required."],
+    unique: true,
+  },
+  image: {
+    type: String,
+    required: [true, "Image is required."],
+  },
+  description: {
+    type: String,
+    required: [true, "Description is required."],
+  },
+  location: [
     {
-        title: {
-            type: String,
-            required: [true, "Title is required."],
-            unique: true,
-        },
-        image: {
-            type: String,
-            required: [true, "Image is required."],
-        },
-        description: {
-            type: String,
-            required: [true, "Description is required."],
-        },
-        location: [{
-            city: { type: String, required: [true, "City is required."] },
-            neighborhood: { type: String },
-            adress: { type: String, required: [true, "Adress is required."] }
-        }],
-        specs: Array,
-        reviews: [{
-            type: Schema.Types.ObjectId,
-            ref: "Review",
-        }]
+      city: { type: String, required: [true, "City is required."] },
+      neighborhood: { type: String },
+      address: { type: String, required: [true, "Address is required."] },
     },
-);
+  ],
+  specs: Array,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
 
 const Cafe = model("Cafe", cafeSchema);
 
