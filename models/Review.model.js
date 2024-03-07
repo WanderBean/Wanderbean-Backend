beea53449b9c8ebd571fcfa1ecee7a0bd1ac9597
arisar86 <ari.sariannidis@gmail.com>
2024-03-07 20:39:31 +0100

    review model small typo change

--- models/Review.model.js
+++ models/Review.model.js
@@ -1,22 +1,20 @@
 const { Schema, model } = require("mongoose");
 
-const reviewSchema = new Schema(
-    {
-        title: {
-            type: String,
-            required: [true, "Title is required."],
-        },
-        description: {
-            type: String,
-            required: [true, "Description is required."],
-        },
-        stars: { type: String, enum: ["1", "2", "3", "4", "5"]}, //// Check this!
-        user: {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-        },
-    },
-);
+const reviewSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required."],
+  },
+  content: {
+    type: String,
+    required: [true, "Content is required."],
+  },
+  stars: { type: String, enum: ["1", "2", "3", "4", "5"] }, //// Check this!
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
 
 const Review = model("Review", reviewSchema);
 
